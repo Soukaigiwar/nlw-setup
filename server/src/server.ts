650ac391f1651c10fd60d1a7141d2eb8178eb8d0
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-18 23:54:44 -0300

    finish back-end setup

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,1 +1,22 @@
-console.log("Olá mundo!");
+import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
+import cors from '@fastify/cors'
+
+const PORT = 3333
+
+const app = fastify()
+const prisma = new PrismaClient()
+
+app.register(cors)
+
+app.get('/', () => {
+    return "olá"
+    
+})
+
+app.listen({
+    port: PORT,
+}).then(() => {
+    console.log(`Server is running on port ${PORT} \nBrower access: http://localhost:3333`);
+    
+})

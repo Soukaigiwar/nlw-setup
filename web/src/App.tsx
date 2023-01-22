6be8fb53bed7b1c9c5cfaca27e907010e6729ee9
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-22 11:43:58 -0300

    add header component

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,18 @@
+import { Header } from './components/Header'
 import './styles/global.css'
-import Habit from "./components/Habit"
 
-export default function App() {
+
+
+
+export function App() {
 
   return (
     <>
-      <Habit completed={3} />
-      <Habit completed={10}/>
-      <Habit completed={20}/>
+      <div className='w-screen h-screen flex justify-center items-center'>
+        <div className='w-full max-w-5xl px-6 flex flex-col gap-16'>
+          <Header />
+        </div>
+      </div>
     </>
   )
 }

587bc9fe38b974cade83cb8a2ed11f080e5798c1
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-22 02:00:08 -0300

    add get habit by date route

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,5 +27,48 @@ export async function appRoutes(app: FastifyInstance) {
                 }
             }
         })
+
+    })
+
+    app.get('/day', async (request) => {
+        const getDayParams = z.object({
+            date: z.coerce.date()
+        })
+
+        const { date } = getDayParams.parse(request.query)
+        
+        const parsedDate = dayjs(date).startOf('day')
+        
+        const weekDay = parsedDate.get('day')
+        console.log(date, parsedDate);
+
+        const possibleHabits = await prisma.habit.findMany({
+            where: {
+                created_at: {
+                    lte: date
+                },
+                weekDays: {
+                    some: {
+                        week_day: weekDay
+                    }
+                }
+            }
+        })
+
+        const day = await prisma.day.findUnique({
+            where: {
+                date: parsedDate.toDate()
+            },
+            include: {
+                dayHabits: true
+            }
+        })
+
+        const completedHabits = day?.dayHabits.map(dayHabit => dayHabit.habit_id)
+
+        return {
+            possibleHabits,
+            completedHabits
+        }
     })
 }

0db18942ec9fbf3d71f3ce7dff70ae9d9bc13438
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-22 14:01:15 -0300

    add vector-icons to project

---
+++ mobile/src/components/Header.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+import { Feather } from "@expo/vector-icons"
+import Logo from "../assets/logo.svg"
+
+export function Header() {
+    return (
+        <View className="w-full flex-row items-center justify-between">
+            <Logo />
+            <Feather
+                name="plus"
+                color="white"
+                size={20}
+            />
+        </View>
+    )
+}

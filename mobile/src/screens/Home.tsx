b4153adf5df89fdf7371a721d270a5ed6a1953e1
Sergio Mello <54086765+Soukaigiwar@users.noreply.github.com>
2023-01-22 22:35:30 -0300

    added habit screen with header text and progressbar

--- mobile/src/screens/Home.tsx
+++ mobile/src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { Text, View, ScrollView } from "react-native";
 import { HabitDay, DAY_SIZE } from "../components/HabitDay";
 import { Header } from "../components/Header";
@@ -9,6 +10,8 @@ const minimunSummaryDatesSizes = 21 * 5
 const amountOfDaysToFill = minimunSummaryDatesSizes - datesFromYearStart.length
 
 export function Home() {
+    const { navigate } = useNavigation()
+
     return (
         <View className="flex-1 bg-background px-8 pt-16">
             <Header />
@@ -29,13 +32,14 @@ export function Home() {
 
             <ScrollView
                 showsVerticalScrollIndicator={false}
-                contentContainerStyle={{ paddingBottom: 100 }}
+                contentContainerStyle={{ paddingBottom: 32 }}
             >
                 <View className="flex-row flex-wrap">
                     {
                         datesFromYearStart.map(date => (
                             <HabitDay
                                 key={date.toISOString()}
+                                onPress={() => navigate('habit', { date: date.toISOString() })}
                             />
                         ))
                     }
